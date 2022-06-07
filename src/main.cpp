46b55d554fc4a47e75a1e42c266ec1a4a02924ff
AhmedAmrAwadallahK8 <AwadallahAhmed25@gmail.com>
2022-06-06 22:04:14 -0700

    Created all Entry types and tested

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 
 #include "DataBaseObjects\int_entry.h"
+#include "DataBaseObjects\double_entry.h"
+#include "DataBaseObjects\char_entry.h"
+#include "DataBaseObjects\string_entry.h"
 
 int main(){
     Int_Entry a(2);
-    // Entry b(2.0);
-    // Entry c('a');
-    // Entry d("Hello");
+    Double_Entry b(2.3);
+    Char_Entry c('a');
+    String_Entry d("Hello");
 
     std::cout << "Int Test| Type: " << a.get_type() << " Value: " << a.get_data() << std::endl;
-    // std::cout << "Dbl Test: " << b.get_data() << std::endl;
-    // std::cout << "Char Test: " << c.get_data() << std::endl;
-    // std::cout << "String Test: " << d.get_data() << std::endl;
+    std::cout << "Double Test| Type: " << b.get_type() << " Value: " << b.get_data() << std::endl;
+    std::cout << "Char Test| Type: " << c.get_type() << " Value: " << c.get_data() << std::endl;
+    std::cout << "String Test| Type: " << d.get_type() << " Value: " << d.get_data() << std::endl;
 
 
     std::cout << "Program Execution Complete";
